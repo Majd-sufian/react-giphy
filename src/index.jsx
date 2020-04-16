34889e7eeed70c1f69f1858618db4885f35bac52
Majd Sufian <majdnewsufian@gmail.com>
2020-04-16 04:56:28 +0200

    add click event

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+class Hello extends Component { // A stateful component needs to
+  constructor(props) { // be promoted into a class
+    super(props);
+    this.state = { 
+    	clicked: false,
+    	counter: 0,
+	}; // defines initial state
+  }
+
+  handleClick = () => {
+  	this.setState({ 
+  		clicked: !this.state.clicked,
+  		counter: this.state.counter +1,
+	});
+  } 
+
+  render() {
+    return (  
+		<h1 className={this.state.clicked ? "clicked" : ""} onClick={this.handleClick}>
+			Hello, {this.props.name} {this.state.counter}
+		</h1> 
+    );
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
